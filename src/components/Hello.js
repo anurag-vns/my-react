62d7999d1a98f177e3832ca549172db05b38930b
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-07-04 22:00:20 +0530

    set state used

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,20 +1,12 @@
 import React,{useState} from "react";
 function Hello(){
     const [count, setCount] = useState(0);
-    const[sub,setSubt]=useState(0);
-	const [a, setAdd] = useState(0);
-    const[b, setSubtraction]=useState(0);
     
 
     return<p>
-        <h2>Hello Count</h2>
-        <h2>Count = {count}</h2>
-	<button onClick={() => setCount(count + 2)}>
-        Counter
-      </button>
-      <h2>Hello sub</h2>
-      <h2> sub={sub}</h2>
-      <button onClick={()=>setSubt(sub-1)}>Subtraction</button>
+      <h2>Hello Count</h2>
+      <h2>count={count}</h2>
+      <button onClick={()=>setCount(count+1)}>Counting</button>
 
     </p>;
 }
