b1a6b9c46ec412bf5193f11960d5a19c9a061830
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-07-05 21:43:25 +0530

    Event Handling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import HelloForm from './components/Hello';
 import Hello from './components/Hello';
  
 function App(props) {
   
   return (
     <div className="App">
-      <Hello></Hello>
+      <HelloForm></HelloForm>
      
 	</div>);
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,25 +1,19 @@
 import React,{useState} from "react";
-function Hello(props){
-    const [count, setCount] = useState(0);
-    const[num1,setAdd1]=useState(0);
-    const[num2,setAdd2]=useState(0);
+function HelloForm(props){
+    const [enteredamount, setenteredamount] = useState(0);
+    const amountchangehandler=(Event)=>
+    {
+        console.log("Amount Changed");
+        console.log(Event.target.value);
+        setenteredamount(Event.target.value);
+    };
+
+    return <form>
+        <label>Amount</label>
+        <input type="text" onChange={amountchangehandler}/>
+        <h1>{enteredamount}</h1>
+        
+    </form>
     
-
-    return<p>
-      <h2>Hello Count</h2>
-      <h2>count={count}</h2>
-      <button onClick={()=>setCount(count+1)}>Counter</button>
-      <h2>First Number</h2>
-      <input type="Number" onChange={Event=>setAdd1(Event.target.value)}/>
-      <h2>Second Number</h2>
-      <input type="Number" onChange={Event=>setAdd2(Event.target.value)}/>
-<h2>{num1}</h2>
-<h2>{num2}</h2>
-<h2>TOTAL  {Number(num1)+Number(num2)}</h2>
-<h2>SUBTRACTION{Number(num1)-Number(num2)}</h2>
-<h2>MULTIPLICATION{Number(num1)*Number(num2)}</h2>
-<h2>DIVIDE{Number(num1)/Number(num2)}</h2>
-
-    </p>;
 }
-export default Hello;
+export default HelloForm;
