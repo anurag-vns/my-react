b1ed2f75f8b3a64d7d670352b853de8f9697a5db
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-07-16 16:08:54 +0530

    Use the JavaScript spread operator to update only the color of the car

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,12 +2,19 @@ import React,{useState} from "react";
 function MyCar()
 {
   const[car, setCar]= useState({brand:"BMW",Model:"Mustong", Year:"2022",color:"Red"});
+
+  const updatecolor=()=>{
+    setCar(previousstate =>{
+      return{...previousstate,color:"blue"}
+    });
+  }
   return(
 <>
     <h1>My Car Name is {car.brand}</h1>
     <p>
       it is model  {car.Model} and color is {car.color} from  {car.Year}.
     </p>
+    <button type="button" onClick={updatecolor}>Blue</button>
    </>
   )
 }
