d7ae18bccb835133d508731ec7ae399305d6791a
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-07-17 17:38:28 +0530

    Beginning Form Submission

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import HelloCalculator from './components/Hello';
 import Timer from './components/Hello';
 import Hello from './components/Hello';
  
 function App(props) {
   
   return (
-    <div className="App">
-      <Timer></Timer>
-     
-	</div>);
+    <div className="container pt-5 bg-primary text-white text-center" id="root">
+      <div class="row">
+        <div class="col - sm-1"></div>
+        <div class="col- sm-10 bg-hite">
+      
+      <HelloCalculator></HelloCalculator>
+      </div>
+     <div class="col-sm-1"></div>
+	</div>
+  </div>
+  );
 }
 export default App;
 
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,16 +1,34 @@
 import React,{useState, useEffect} from "react";
-function Timer() {
+function HelloCalculator(props) {
   const [count, setCount] = useState(0);
+const[num1,setAdd1]=useState(0);
+const[num2,setAdd2]=useState(0);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 500);
-  });
+return<form>
+  <div class="form-group">
+  <label for="Item">Item</label>
+  <input type="email" class="form-control" placeholder="Enter Item" id="Item"/>
+  </div>
 
-  return <h1> Count Starting {count} times!</h1>;
+  <div class="form-group">
+    <label for="amount">Amount</label>
+    <input type="amount" class="form-control" placeholder="Enter Amount" id="Amount"/>
+  </div>
+
+  <div>
+    <label for="date">Date</label>
+    <input type="date" class="form-control" placeholder="Enter Date" id ="Date"/>
+  </div>
+
+  <div>
+    <button type="Submit" class="btn btn-primary">Submit</button>
+  </div>
+
+</form>;
+
+
 }
 
-  export default Timer;
+  export default HelloCalculator;
 
 
