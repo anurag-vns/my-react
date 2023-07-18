e1bafb9f8193eb566b75b0b2d3f584e1140e93ea
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-07-18 19:11:04 +0530

    Handlers for all changes
    Added handlers for all textfields

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,12 +1,42 @@
 import React,{useState} from "react";
-function HelloCalculator(props) {
-  const [expensesdata, setExpenses] = useState({item:'',Amount:0,tdate:''});
+function HelloCalculator() {
+ 
+  const [expensesdata, setExpenses] = useState({
+    item:'',
+    Amount:0,
+    tdate:''
+  });
 
   const itemChangeHandler=(Event)=>
   {
-    setExpenses = (Event.target.value);
-   console.log(Event.target.value);
-  };
+    setExpenses ( (prevState)=>{
+   return{...prevState, item:Event.target.value};
+  });
+
+  console.log(expensesdata);
+};
+
+const amountChangeHandler=(Event)=>
+{
+  setExpenses((prevState)=>{
+  return{...prevState,amount:Number(Event.target.value)};
+
+});
+console.log(expensesdata);
+};
+
+const dateChangeHandler=(Event)=>
+{
+  setExpenses( (prevState)=>{
+    return{...prevState,tdate: new Date(Event)};
+
+  });
+console.log(expensesdata);
+};
+  
+
+  
+
 
 return<form>
   <div class="form-group">
@@ -16,12 +46,12 @@ return<form>
 
   <div class="form-group">
     <label for="amount">Amount</label>
-    <input type="amount" class="form-control" placeholder="Enter Amount" id="Amount"/>
+    <input type="amount" class="form-control" placeholder="Enter Amount" id="Amount" onChange={amountChangeHandler}/>
   </div>
 
   <div>
     <label for="tdate">Date</label>
-    <input type="date" class="form-control" placeholder="Enter Date" id ="tDate"/>
+    <input type="date" class="form-control" placeholder="Enter Date" id ="tdate" onChange={dateChangeHandler}/>
   </div>
 
   <div>
