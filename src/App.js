e144b480a8554b1c2d71ac62094b9a9d9aae6bf8
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-07-28 21:53:09 +0530

    default exports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React1 from './React Tutorial/Countable';
 import Gallary from './React Tutorial/Profile';
  
 function App(props) {
---
+++ src/React Tutorial/Gallery.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Profile } from "./Profile";
+ export function Gallery(){
+    return(
+        <section>
+            <Profile />
+        </section>
+    );
+}
