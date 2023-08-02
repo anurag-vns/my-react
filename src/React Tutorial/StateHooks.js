7e5659667d53c5200d960e7bd5f8b4f1cd78137c
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-08-02 22:05:28 +0530

    Update only the color of the car
    Use the JavaScript spread operator to update only the color of the car

--- src/React Tutorial/StateHooks.js
+++ src/React Tutorial/StateHooks.js
@@ -10,13 +10,17 @@ function Car(){
         year:"2023"
     });
 
-
+const updatecolor=()=>{
+    setCar(previousState=>{
+        return{...previousState,color:"Green"}
+    });
+}
 
 return(
     <>
     <h1> My  {car.brand}</h1>
     <p> color is {car.color} and model is {car.model} from {car.brand}</p>
-
+<button type="button" onClick={updatecolor}>Update Color</button>
 </>
 );
 }
