d760f59e48907db8cc49b050907001b8ed7c3473
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-08-02 20:52:31 +0530

    Example hook Color Change by using Buttons

--- src/components/State.js
+++ src/components/State.js
@@ -6,8 +6,13 @@ const[color, setColor]=useState("Green");
 
 
 return(
+    <>
     <h1>My favorite  Color is {color}</h1>
+    <button  type="button" onClick={()=>setColor("blue")}>Blue</button>
+    <button type="button" onClick={()=> setColor("Red")}>Red</button>
+    <button type="button" onClick={()=>setColor("Pink")}>Pink</button>
 
+</>
 );
 }
 export default FavoriteColor;
