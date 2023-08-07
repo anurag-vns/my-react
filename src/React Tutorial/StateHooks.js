9b60d76fe9aa1d91d65b1e45d25105cb98e1911a
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2023-08-07 21:01:07 +0530

    usestate

--- src/React Tutorial/StateHooks.js
+++ src/React Tutorial/StateHooks.js
@@ -1,27 +1,16 @@
 import React from "react";
 import { useState } from "react";
 
-function Car(){
-
-    const[car, setCar]= useState({
-        brand:"BMW",
-        color:"Black",
-        model:"BM2023",
-        year:"2023"
-    });
-
-const updatecolor=()=>{
-    setCar(previousState=>{
-        return{...previousState,color:"Green"}
-    });
-}
-
-return(
-    <>
-    <h1> My  {car.brand}</h1>
-    <p> color is {car.color} and model is {car.model} from {car.brand}</p>
-<button type="button" onClick={updatecolor}>Update Color</button>
+function Counter(){
+    const[count,setCount]=useState("0");
+    return(
+<>
+<h1>{count}</h1>
+<button onClick={()=>{
+    setCount=(count+1);
+}}>+3</button>
+ 
 </>
-);
+    )
 }
-export default Car;
+export default Counter;
